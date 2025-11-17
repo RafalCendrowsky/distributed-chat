79befda39be68fe0305280b6f85ba749a2d67b16
Rafał Cendrowski <r.cendrowski@uw.edu.pl>
2025-11-17 16:34:20 +0100

    update project scaffolding

---
+++ common/src/main/java/com/rafalcendrowski/common/KafkaTopics.java
@@ -0,0 +1,9 @@
+package com.rafalcendrowski.common;
+
+public final class KafkaTopics {
+    private KafkaTopics() {
+    }
+
+    public static final String MESSAGES_NEW = "messages.new";
+    public static final String MESSAGES_PROCESSED = "messages.processed";
+}
